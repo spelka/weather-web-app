77f50c9a10b9a13f76fd9a0e6f808fd1841efe80
spelka <sebastian.pelka@hotmail.com>
2019-02-01 12:47:22 -0800

    Displaying 5-day forecast correctly.

--- src/app/forecast.ts
+++ src/app/forecast.ts
@@ -11,4 +11,35 @@ export interface Forecast
     temperatureHigh: number;
     temperatureLow: number;
     humidity: number;
+
+    day1_date: number;
+    day1_month: number;
+    day1_icon: string;
+    day1_temperatureHigh: number;
+    day1_temperatureLow: number;
+
+    day2_date: number;
+    day2_month: number;
+    day2_icon: string;
+    day2_temperatureHigh: number;
+    day2_temperatureLow: number;
+
+    day3_date: number;
+    day3_month: number;
+    day3_icon: string;
+    day3_temperatureHigh: number;
+    day3_temperatureLow: number;
+
+    day4_date: number;
+    day4_month: number;
+    day4_icon: string;
+    day4_temperatureHigh: number;
+    day4_temperatureLow: number;
+
+    day5_date: number;
+    day5_month: number;
+    day5_icon: string;
+    day5_temperatureHigh: number;
+    day5_temperatureLow: number;
+
 }
--- src/app/local-forecast/local-forecast.component.ts
+++ src/app/local-forecast/local-forecast.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from './../weather/weather.service'
 import { Forecast } from './../Forecast';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-local-forecast',
@@ -12,7 +13,8 @@ export class LocalForecastComponent implements OnInit {
   constructor(private weather:WeatherService) { }
 
   forecast : Forecast;
-
+  weatherData : Observable<Object>;
+  
   monthNames = [
     "January", "February", "March",
     "April", "May", "June", "July",
@@ -22,30 +24,71 @@ export class LocalForecastComponent implements OnInit {
 
   ngOnInit()
   {
-    console.log("Calling LocalForecastComponent.ngOnInit()")
+    console.log("Calling LocalForecastComponent.ngOnInit()");
+    this.getWeatherData();
     this.getForecast();
+  }
+
+  getWeatherData()
+  {
+    this.weatherData = this.weather.getWeatherData();
   }
 
   getForecast()
   {
-    console.log("Calling LocalForecastComponent.getForecast()")
+    console.log("Calling LocalForecastComponent.getForecast()");
       //subscribe to the incoming observable data and clone fields into the Forecast object
-      this.weather.getWeatherData()
-      .subscribe((data: any) => this.forecast = {
-        //Location
-        location: data['timezone'],
-        //Current (Day 0) Forecast
-        summary: data.currently['summary'],
-        date: new Date(data.currently['time'] * 1000).getDate(),
-        month: new Date(data.currently['time'] * 1000).getMonth(),
-        year: new Date(data.currently['time'] * 1000).getFullYear(),
-        icon: data.currently['icon'],
-        precipProbability: data.currently['precipProbability'],
-        temperature: data.currently['temperature'],
-        temperatureHigh: data.currently['temperatureHigh'],
-        temperatureLow: data.currently['temperatureLow'],
-        humidity: data.currently['humidity']
-      })
-  }
 
+      
+
+      this.weatherData.subscribe((data: any) => this.forecast = {
+        //Current (Today's) Forecast
+        location: data['timezone'],
+        summary: data.currently['summary'],
+        date: new Date(data['currently']['time'] * 1000).getDate(),
+        month: new Date(data['currently']['time'] * 1000).getMonth(),
+        year: new Date(data['currently']['time'] * 1000).getFullYear(),
+        icon: data['currently']['icon'],
+        precipProbability: data['currently']['precipProbability'],
+        temperature: data['currently']['temperature'],
+        temperatureHigh: data['currently']['temperatureHigh'],
+        temperatureLow: data['currently']['temperatureLow'],
+        humidity: data['currently']['humidity'],
+
+        //+1 Day (Tomorrow's) Forecast
+        day1_date: new Date(data['daily']['data'][1]['time'] * 1000).getDate(),
+        day1_month: new Date(data['daily']['data'][1]['time'] * 1000).getMonth(),
+        day1_icon: data['daily']['data'][1]['icon'],
+        day1_temperatureHigh: data['daily']['data'][1]['temperatureHigh'],
+        day1_temperatureLow: data['daily']['data'][1]['temperatureLow'],
+
+        //+2 Day Forecast
+        day2_date: new Date(data['daily']['data'][2]['time'] * 1000).getDate(),
+        day2_month: new Date(data['daily']['data'][2]['time'] * 1000).getMonth(),
+        day2_icon: data['daily']['data'][2]['icon'],
+        day2_temperatureHigh: data['daily']['data'][2]['temperatureHigh'],
+        day2_temperatureLow: data['daily']['data'][2]['temperatureLow'],
+
+        //+3 Day Forecast
+        day3_date: new Date(data['daily']['data'][3]['time'] * 1000).getDate(),
+        day3_month: new Date(data['daily']['data'][3]['time'] * 1000).getMonth(),
+        day3_icon: data['daily']['data'][3]['icon'],
+        day3_temperatureHigh: data['daily']['data'][3]['temperatureHigh'],
+        day3_temperatureLow: data['daily']['data'][3]['temperatureLow'],
+
+        //+4 Day Forecast
+        day4_date: new Date(data['daily']['data'][4]['time'] * 1000).getDate(),
+        day4_month: new Date(data['daily']['data'][4]['time'] * 1000).getMonth(),
+        day4_icon: data['daily']['data'][4]['icon'],
+        day4_temperatureHigh: data['daily']['data'][4]['temperatureHigh'],
+        day4_temperatureLow: data['daily']['data'][4]['temperatureLow'],
+
+        //+5 Day Forecast
+        day5_date: new Date(data['daily']['data'][5]['time'] * 1000).getDate(),
+        day5_month: new Date(data['daily']['data'][5]['time'] * 1000).getMonth(),
+        day5_icon: data['daily']['data'][5]['icon'],
+        day5_temperatureHigh: data['daily']['data'][5]['temperatureHigh'],
+        day5_temperatureLow: data['daily']['data'][5]['temperatureLow']
+      });
+  }
 }
